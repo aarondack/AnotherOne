ebb36cee42c810512cc089131f0bff19fb9557df
alexcatanzaro <alexcatanzaro@gmail.com>
2017-07-08 18:53:16 -0500

    Top and bottom of the List of Programming langs wiki

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const got = require('got');
 
-const LIST_OF_PROGRAMMING_LANGUAGES = 'https://en.wikipedia.org/w/api.php?action=query&titles=List_of_programming_languages&prop=revisions&rvprop=content&format=json';
+const LIST_OF_PROGRAMMING_LANGUAGES = 'https://en.wikipedia.org/w/api.php?action=query&titles=List_of_programming_languages&prop=links&pllimit=500&format=json';
+const LIST_OF_PROGRAMMING_LANGUAGES_2='https://en.wikipedia.org/w/api.php?action=query&titles=List_of_programming_languages&prop=links&pllimit=213&pldir=descending&format=json';
 const PAGE_ID = '144146';
-const REVISIONS = '*';
+let top = [];
+let bottom = []; 
 
 got(LIST_OF_PROGRAMMING_LANGUAGES, { json: true })
   .then( response => {
-    const body = response.body;
-    console.log(response.body.query.pages[PAGE_ID]);
+    topHalf = response.body.query.pages[PAGE_ID].links;
 });
+
+got(LIST_OF_PROGRAMMING_LANGUAGES_2, { json: true })
+  .then( response  => {
+    bottomHalf = response.body.query.pages[PAGE_ID].links;
+  });
