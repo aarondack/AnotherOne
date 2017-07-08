71f4e31939abbd091bc5d45e493817c56713b031
Dack <adackbusiness@gmail.com>
2017-07-08 17:52:07 -0400

    Revisions

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const got = require('got');
 
-got('https://en.wikipedia.org/w/api.php?action=query&titles=List_of_programming_languages&prop=revisions&rvprop=content&format=json')
+const LIST_OF_PROGRAMMING_LANGUAGES = 'https://en.wikipedia.org/w/api.php?action=query&titles=List_of_programming_languages&prop=revisions&rvprop=content&format=json';
+const PAGE_ID = '144146';
+const REVISIONS = '*';
+
+got(LIST_OF_PROGRAMMING_LANGUAGES, { json: true })
   .then( response => {
-    console.log(response.body);
+    const body = response.body;
+    console.log(response.body.query.pages[PAGE_ID]);
 });
