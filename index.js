7bad3de4529b95263432f5290d2411d5a4b3488b
alexcatanzaro <alexcatanzaro@gmail.com>
2017-07-10 21:23:33 -0500

    Split list into top and bottom

--- index.js
+++ index.js
@@ -6,12 +6,38 @@ const PAGE_ID = '144146';
 let top = [];
 let bottom = []; 
 
-got(LIST_OF_PROGRAMMING_LANGUAGES, { json: true })
+async function getTop() {
+ return got(LIST_OF_PROGRAMMING_LANGUAGES, { json: true })
   .then( response => {
     topHalf = response.body.query.pages[PAGE_ID].links;
+    topHalf.forEach(link => {
+      top.push(link.title);
+    })
+    return top;
 });
+};
 
-got(LIST_OF_PROGRAMMING_LANGUAGES_2, { json: true })
+async function getBottom() {
+  return got(LIST_OF_PROGRAMMING_LANGUAGES_2, { json: true })
   .then( response  => {
     bottomHalf = response.body.query.pages[PAGE_ID].links;
-  });
+    bottomHalf.forEach(link => {
+      bottom.push(link.title);
+    })
+    return bottom;
+});
+};
+
+async function awaitTopNBottom() {
+  let topArray =  await getTop();
+  let bottomArray = await getBottom();
+
+  let programmingLanguages = [...topArray, ...bottomArray];
+  return programmingLanguages;
+};
+
+awaitTopNBottom().then(value => {
+  console.log(value);
+});
+
+
