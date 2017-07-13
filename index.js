4726e00842d86a66f27e098be5080dccb5a7f843
Dack <adackbusiness@gmail.com>
2017-07-13 14:34:18 -0400

    remove async

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const LIST_OF_PROGRAMMING_LANGUAGES = 'https://en.wikipedia.org/w/api.php?action
 const LIST_OF_PROGRAMMING_LANGUAGES_2='https://en.wikipedia.org/w/api.php?action=query&titles=List_of_programming_languages&prop=links&pllimit=213&pldir=descending&format=json';
 const PAGE_ID = '144146';
 
-async function getTop() {
+export function getTop() {
  return got(LIST_OF_PROGRAMMING_LANGUAGES, { json: true })
   .then( response => {
     topHalf = response.body.query.pages[PAGE_ID].links;
@@ -12,7 +12,7 @@ async function getTop() {
 });
 };
 
-async function getBottom() {
+export function getBottom() {
   return got(LIST_OF_PROGRAMMING_LANGUAGES_2, { json: true })
   .then( response  => {
     bottomHalf = response.body.query.pages[PAGE_ID].links;
